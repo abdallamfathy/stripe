924a5bdd0452f3d1b343c5dffb1ce5d459a5f790
Abdallah mohamed <abdallamfathy@gmail.com>
2022-03-13 16:49:47 +0200

    full project

--- src/context.js
+++ src/context.js
@@ -6,19 +6,32 @@ import sublinks from './data'
 
 export const AppProvider = ({children}) => {
     const [isSidebarOpen,setIsSidebarOpen] = useState(false);
-    const [isSubmenuOpen,setIsSubmenuOpen] = useState(true);
+    const [isSubmenuOpen,setIsSubmenuOpen] = useState(false);
+    const [location,setLocation] = useState({});
+    const [page,setPage] = useState({page:"",links:[]});
 
     const openSidebar = () =>{
         setIsSidebarOpen(true);
     };const closeSidebar = () =>{
         setIsSidebarOpen(false);
-    };const openSubmenu = () =>{
+    };const openSubmenu = (text,coordinates) =>{
+        const page = sublinks.find((link)=> link.page === text);
+        setPage(page);
+        setLocation(coordinates);
         setIsSubmenuOpen(true);
     };const closeSubmenu = () =>{
         setIsSubmenuOpen(false);
     }
 
-    return <AppContext.Provider value={{isSubmenuOpen, isSidebarOpen,openSubmenu,closeSubmenu,openSidebar,closeSidebar}}>
+    return <AppContext.Provider value={{
+        isSubmenuOpen,
+        isSidebarOpen,
+        openSubmenu,
+        closeSubmenu,
+        openSidebar,
+        closeSidebar,
+        location,
+        page}}>
         {children}
     </AppContext.Provider>
 }
